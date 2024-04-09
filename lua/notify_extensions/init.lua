ace0a3450c7f35dd48a6b2f34bd99f2b8bee341a
zerochae <zerochae@kakao.com>
2024-04-09 10:13:51 +0900

    🐛 Fix: 오타 수정

--- lua/notify_extensions/init.lua
+++ lua/notify_extensions/init.lua
@@ -23,7 +23,7 @@ M.setup = function(config)
   local notify_extensions = {}
   local lsp_loader_enable = config.lsp_loader.enable or default_config.lsp_loader.enable
   local plugin_updator_enable = config.plugin_updator.enable or default_config.plugin_updator.enable
-  local ignore_lsp = config.lsp_loader.igrnoe or default_config.lsp_loader.ignore
+  local ignore_lsp = config.lsp_loader.ignore or default_config.lsp_loader.ignore
   local init = function(extensions)
     for _, extension in pairs(extensions) do
       extension()
